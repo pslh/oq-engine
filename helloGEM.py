de5cf876f49583e373fddf96371af16ad8f853bd
Laurentiu <laurentiudanciu@gmail.com>
2010-08-02 16:19:44 +0200

    a simple python script

---
+++ helloGEM.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+helloworld.py
+
+Created by Laurentiu Danciu on 2010-08-02.
+Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+print "Hello, GEM!" 
