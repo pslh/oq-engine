d5794e9e54b703aae82244013b0514a3d754416e
Aurea Moemke <aurea.moemke@sed.ethz.ch>
2010-10-04 17:57:52 +0200

    Add Populate Calc Grps/Owners using Django ORM

--- opengem/db/hazcurve_to_db/populateCalcGrpOwners.py
+++ opengem/db/hazcurve_to_db/populateCalcGrpOwners.py
@@ -1,88 +1,117 @@
 #!/usr/bin/env python
 
-import datetime
-from django.db import models
-#from hazcurve_to_db.hazcurvetodb.models import Calculationgroup
-#from hazcurve_to_db.hazcurvetodb.models import Calculationowner
+# This file shows how to use the OR mapping to directly create
+# Calculationgroup records and related Calculationowner records
+# For testing/illustration purposes only.
+# author: Aurea Moemke
+# date: 4 October 2010
 
-#or from django.db import models
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from psycopg2 import IntegrityError
+
+import datetime
+from hazcurvetodb.models import Calculationgroup, Calculationowner
 
 # To create database records
 cg = Calculationgroup(cgcode="OPENGEMDEV", cgname="OPENGEMDEV", 
         cgdesc="Opengem Development Team", cgauthlevel="1", 
-        cgadddate=datetime.datetime.now(),cgremarks="Opengem Development Team, ETH Zurich, Zurich, Switzerland and EU Center, Pavia, Italy")
+        cgadddate=datetime.datetime.now(), cgremarks="Opengem Development Team,\        ETH Zurich, Zurich, Switzerland and EU Center, Pavia, Italy")
 cg.save()
 cg = Calculationgroup(cgcode="SHAREDEV", cgname="SHAREDEV", 
-        cgdesc="Share Development Team", cgauthlevel="1", cgadddate=datetime.datetime.now(), cgremarks="Share Development Team, \
+        cgdesc="Share Development Team", cgauthlevel="1", \
+        cgadddate=datetime.datetime.now(), cgremarks="Share Development Team, \
         ETH Zurich, Zurich, Switzerland")
 cg.save()
 
-#If create same, with same primary key, it will be overwritten so first filter, then if non-existent create
+# If record with same primary key is written, it will overwrite an existing
+# record with same primary key. Therefore, first filter, then if non-existent 
+# create
 # To delete
-#cg = Calculationgroup.objects.filter(pk="")
-#cg.delete()
+# cg = Calculationgroup.objects.filter(pk="")
+# cg.delete()
 
-# to show all objects
+# To show all objects
 Calculationgroup.objects.all()
-#Out[44]: [<Calculationgroup: OPENGEMDEV>, <Calculationgroup: SHAREDEV>]
+# Out[44]: [<Calculationgroup: OPENGEMDEV>, <Calculationgroup: SHAREDEV>]
 
-# to get by a filter
+# To get a record using a filter, either pk for primary key or field name itself
 cg = Calculationgroup.objects.get(pk="SHAREDEV")
 cg = Calculationgroup.objects.get(cgname="OPENGEMDEV")
 
-#Create related keys
+# Create Calculation Owner records corresponding to Calculation Group
 cg.calculationowner_set.all()
 # returns []
 
-#Creates 3 calculation owner records related (auto create of relation to calculation group)
-# first get calcuation group object
+# Creates 5 calculation owner records (foreign key field is automatically 
+# created) 
+# First get Calculation group object
 cg = Calculationgroup.objects.get(cgname="OPENGEMDEV")
-# set relations to calc owner
-cg.calculationowner_set.create(cocode="AM", coname="AUREA MOEMKE", 
-        codesc="DB Architect, SED, ETH Zurich", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Opengem dev team, top level")
-cg.calculationowner_set.create(cocode="DM", coname="DAMIANO MONELLI", 
-        codesc="Hazard/Risk Scientist, SED, ETH Zurich", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Opengem dev team, top level")
-cg.calculationowner_set.create(cocode="PK", coname="PHILIPP KAESTLI", 
-        codesc="IT Coordinator, SED, ETH Zurich", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Opengem dev team, top level")
 
+# Set relations to calc owner
+cg.calculationowner_set.create(cocode="JMK", coname="JOSHUA MCKENTY",\
+        codesc="OPENGEM IT MANAGER, EU Center, Pavia, Italy", coauthlevel="1",\
+        coadddate=datetime.datetime.now(),\
+        coremarks="With Opengem dev team, top level")
+
+cg.calculationowner_set.create(cocode="AM", coname="AUREA MOEMKE",\
+        codesc="DB Architect, SED, ETH Zurich", coauthlevel="1",\
+        coadddate=datetime.datetime.now(), \
+        coremarks="With Opengem dev team, top level")
+
+cg.calculationowner_set.create(cocode="DM", coname="DAMIANO MONELLI", \
+        codesc="Hazard/Risk Scientist, SED, ETH Zurich", coauthlevel="1",\ 
+        coadddate=datetime.datetime.now(),\
+        coremarks="With Opengem dev team, top level")
+
+cg.calculationowner_set.create(cocode="PK", coname="PHILIPP KAESTLI", \
+        codesc="IT Coordinator, SED, ETH Zurich", coauthlevel="1",\ 
+        coadddate=datetime.datetime.now(),\
+        coremarks="With Opengem dev team, top level")
+
+# Test deletes
 cg = Calculationgroup.objects.get(cgname="SHAREDEV")
-cg.calculationowner_set.create(cocode="AC", coname="ANDREA CERISARA", 
-        codesc="Software Engineer, EU Center, Pavia, Italy", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Opengem dev team, top level")
-cg.calculationowner_set.create(cocode="VS", coname="VITOR SILVA", 
-        codesc="Risk Scientist, EU Center, Pavia, Italy", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Opengem dev team, top level")
 
-#delete and add to opengemdev
+cg.calculationowner_set.create(cocode="AC", coname="ANDREA CERISARA", \
+        codesc="Software Engineer, EU Center, Pavia, Italy", coauthlevel="1", \
+        coadddate=datetime.datetime.now(), \
+        coremarks="With Opengem dev team, top level")
+
+cg.calculationowner_set.create(cocode="VS", coname="VITOR SILVA",\ 
+        codesc="Risk Scientist, EU Center, Pavia, Italy", coauthlevel="1",\
+        coadddate=datetime.datetime.now(), \
+        coremarks="With Opengem dev team, top level")
+
+# Delete from SHAREDEV and add to OPENGEMDEV
 co = Calculationowner.objects.get(pk="AC")
 co.delete()
 co = Calculationowner.objects.get(pk="VS")
-print co
 co.delete()
 
-#NOW add to OPENGEMDEV
+# NOW add to OPENGEMDEV
 cg = Calculationgroup.objects.get(cgname="OPENGEMDEV")
-cg.calculationowner_set.create(cocode="AC", coname="ANDREA CERISARA", 
-        codesc="Software Engineer, EU Center, Pavia, Italy", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Opengem dev team, top level")
-cg.calculationowner_set.create(cocode="VS", coname="VITOR SILVA", 
-        codesc="Risk Scientist, EU Center, Pavia, Italy", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Opengem dev team, top level")
-cg.calculationowner_set.create(cocode="JMK", coname="JOSHUA MCKENTY", 
-        codesc="OPENGEM IT MANAGER, EU Center, Pavia, Italy", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Opengem dev team, top level")
-#Out[73]: < Calculationowner: JOSHUA MCKENTY > 
-#OK, alles funktionert!
-#Add to SHAREDEV
+
+cg.calculationowner_set.create(cocode="AC", coname="ANDREA CERISARA",\
+        codesc="Software Engineer, EU Center, Pavia, Italy", coauthlevel="1",\ 
+        coadddate=datetime.datetime.now(),\
+        coremarks="With Opengem dev team, top level")
+
+cg.calculationowner_set.create(cocode="VS", coname="VITOR SILVA", \
+        codesc="Risk Scientist, EU Center, Pavia, Italy", coauthlevel="1",\ 
+        coadddate=datetime.datetime.now(),\
+        coremarks="With Opengem dev team, top level")
+ 
+# Now add Calculation Owners to SHAREDEV Calculation Group
+
 cg = Calculationgroup.objects.get(cgname="SHAREDEV")
-cg.calculationowner_set.create(cocode="LD", coname="LAURENTIU DANCIU", 
-        codesc="Hazard/Risk Scientist, SED, ETH Zurich", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Share dev team, top level")
-#Out[75]: <Calculationowner: LAURENTIU DANCIU>
-cg.calculationowner_set.create(cocode="RK", coname="RAMAKRISHNAN KRISHNAMURTHY", 
-        codesc="Software Architect, SED, ETH Zurich", coauthlevel="1", 
-        coadddate=datetime.datetime.now(), coremarks="With Share dev team, top level")
+
+cg.calculationowner_set.create(cocode="LD", coname="LAURENTIU DANCIU", \
+        codesc="Hazard/Risk Scientist, SED, ETH Zurich", coauthlevel="1",\ 
+        coadddate=datetime.datetime.now(),\
+        coremarks="With Share dev team, top level")
+cg.calculationowner_set.create(cocode="RK",\
+        coname="RAMAKRISHNAN KRISHNAMURTHY", \
+        codesc="Software Architect, SED, ETH Zurich", coauthlevel="1",\
+        coadddate=datetime.datetime.now(),\
+        coremarks="With Share dev team, top level")
 
