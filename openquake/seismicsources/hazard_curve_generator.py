da330cb4309a9f8955853b727071b9fe6febec77
bwyss <bmwyss@gmail.com>
2010-12-10 16:30:34 +0100

    basic writer to  connection to the db

--- openquake/seismicsources/hazard_curve_generator.py
+++ openquake/seismicsources/hazard_curve_generator.py
@@ -1,20 +1,78 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+
 """ 
 This code is intended to use SQLAlchemy to write hazard curves from the OpenQuake engine to a database. The SQLAlchemy Object Relational Mapper presents a method of associating user-defined Python classes with database tables, and instances of those classes (objects) with rows in their corresponding tables."""
 
 
-class HazardCurveGenerator(object):
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.orm import mapper
+from sqlalchemy import Integer, String
+from openquake import shapes
 
-    def __init__(self, model_id, endBranchLabel, values, imt, timeSpanDuration
-                    imlValues):
-        self.model_id = Model_ID
-        self.endBranchLabel = End_Branch_Label
-        self.values = Values
-        self.imt = IMT
-        self.timeSpanDuration = Time_Span_Duration
+#TODO BW create a 'real' connection to a DB for mapping hazard curves.
+#TODO Change echo to False when you’re ready to put the code into production.
+engine = create_engine('sqlite:///test.db', echo=True)
+"""sqlEngine/connection object, temporary connection to in-memory-only
+       SQLite database"""
+
+#Table definition
+metadata = MetaData(bind=engine)
+#Table generation
+hazard_curve_table = Table('hazard_curve', metadata,
+                        Column('model_id', String, primary_key=True),
+                        Column('end_branch_label', String),
+                        Column('_values', String),
+                        Column('imt', String(40)),
+                        Column('time_span_duration', String),
+                        Column('iml_values', String),
+                    )
+metadata.create_all()
+
+#SQLAlchemy SQL construction method
+
+class HazardCurveGenerator(object):
+    
+    def __init__(self, model_id, end_branch_label, values, imt,
+        time_span_duration, iml_values):
+        self.model_id = model_id
+        self.end_branch_label = end_branch_label
+        self.values = values
+        self.imt = imt
+        self.time_span_duration = time_span_duration
+        self.iml_values = iml_values
         
     def __repr__(self):
-        return "<User('%s','%s', '%s', '%s', '%s')>" % (self.model_id, self.endBranchLabel,
-        self.values, self.imt, self.timeSpanDuration)
+        return "<User('%s','%s', '%s', '%s', '%s')>" % (self.model_id,
+            self.endBranchLabel, self.values, self.imt, self.timeSpanDuration)
+        
+hcg = HazardCurveGenerator('1', 'test_endBranchLabel', 'test_valuse',
+    'test_IMT', 'test_timeSpan', 'test_IMLValues')
+    
+print hcg.model_id
+
+print hcg.end_branch_label
+
+print hcg.values
+print hcg.imt
+print hcg.time_span_duration
+print hcg.iml_values
+
+
+# create an Insert object        
+ins = hazard_curve_table.insert()
+
+# add values to the Insert object
+new_curve = ins.values(model_id=hcg.model_id,
+    end_branch_label=hcg.end_branch_label, _values=hcg.values, imt=hcg.imt,
+    time_span_duration=hcg.time_span_duration, iml_values=hcg.iml_values)
+
+# create a database connection
+conn = engine.connect()
+# add user to database by executing SQL
+conn.execute(new_curve)
+# close the db connection
+conn.close()
         
--- tests/db_mapper_unittest.py
+++ tests/db_mapper_unittest.py
@@ -9,6 +9,8 @@ from openquake import hazard
 class HazardCurveTestCase(unittest.TestCase):
     
     def test_simple_hazard_curve_witten_to_db(self):
-        
-        hazard_curve_test = HazardCurve('FIXED', 'label', [6.0999999999999996, 6.2000000000000002, 6.2999999999999998], 'PGA', '50.0', [1.0, 2.0, 3.0])
+          
+        i.execute(IDmodel='FIXED', endBranchLabel=label,
+        Values='cahnge',
+        IMT='PGA', timeSpanDuration='50.0', IMLValues='change')
         
