96645b79856960b63221bde15b3a4f82831f4aae
bwyss <bmwyss@gmail.com>
2010-12-14 14:37:14 +0100

    brake out three hazard curve tables

--- openquake/seismicsources/hazard_curve_generator.py
+++ openquake/seismicsources/hazard_curve_generator.py
@@ -8,65 +8,89 @@ This code is intended to use SQLAlchemy to write hazard curves from the OpenQuak
 
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy.orm import mapper
+from sqlalchemy.orm import mapper, relationship
 from sqlalchemy import Integer, String
 from openquake import shapes
+from sqlalchemy.ext.declarative import declarative_base
 
 #TODO BW create a 'real' connection to a DB for mapping hazard curves.
 #TODO Change echo to False when you’re ready to put the code into production.
-engine = create_engine('sqlite:///hazard_curve_table_test.db', echo=True)
+#engine = create_engine('sqlite:///hazard_result_table_test.db', echo=True)
 """sqlEngine/connection object, temporary connection to in-memory-only
        SQLite database"""
+engine = create_engine('sqlite:///:memory:', echo=True)
 
-#Table definition
 metadata = MetaData(bind=engine)
-#Table generation
-hazard_curve_table = Table('hazard_curve', metadata,
-                        Column('model_id', String),
-                        Column('end_branch_label', String),
-                        Column('_values', String),
-                        Column('imt', String(40)),
-                        Column('time_span_duration', String),
-                        Column('iml_values', String),
-                    )
-metadata.create_all()
-
+                  
 #SQLAlchemy SQL construction method
-
-class HazardCurveGenerator(object):
+""" The hazard curve need to be split into thee classes: HazardResult,
+HazardCurveList and HazardCurve in order reduce duplicate saved information
+to the DB"""
+Base = declarative_base()
+class HazardResult(object):
+    #Table generation
+    __tablename__ = hazard_result_table
     
-    def __init__(self, model_id, end_branch_label, values, imt,
-        time_span_duration, iml_values):
+    id_key = Column(Integer, primary_key=True),
+    model_id = Column(String),
+    time_span_duration = Column(String),
+    sa_period = Column(String),
+    sa_damping = Column(String)
+    
+    #Table definition
+    def __init__(self, model_id, time_span_duration, sa_period, sa_damping):
+        #self.id_key = id_key
         self.model_id = model_id
-        self.end_branch_label = end_branch_label
-        self.values = values
-        self.imt = imt
         self.time_span_duration = time_span_duration
-        self.iml_values = iml_values
+        self.sa_period = sa_period
+        self.sa_damping = sa_damping
         
     def __repr__(self):
-        return "<User('%s','%s', '%s', '%s', '%s')>" % (self.model_id,
-            self.endBranchLabel, self.values, self.imt, self.timeSpanDuration)
-        
-hcg = HazardCurveGenerator('1', 'test_endBranchLabel', 'test_valuse',
-    'test_IMT', 'test_timeSpan', 'test_IMLValues')
+        return "<User('%s','%s', '%s', '%s', '%s')>" % (self.id_key,
+        self.model_id, self.timeSpanDuration, self.sa_period, self.sa_damping)
+            
+class HazardCurveList(object):
+    #Table generation
+    __tablename__ = hazard_curve_list_table
     
-print hcg.model_id
-print hcg.end_branch_label
-print hcg.values
-print hcg.imt
-print hcg.time_span_duration
-print hcg.iml_values
+    id_key = Column(Integer, primary_key=True),
+    parent_id = Column('parent_id', Integer, ForeignKey('hazard_curve.id_key'))
+    
+    def __init__(self, id_key, parent_id):
+        self.id_key = id_key
+        self.parent_id = parent_id
+    
+class HazardCurve(object):
+    pass
+  
+mapper(HazardResult, hazard_result_table, properties={
+   'children': relationship(HazardCurveList)
+})
 
+mapper(HazardCurveList, hazard_curve_list_table)
 
+metadata.drop_all()
+metadata.create_all()     
+        
+hr = HazardResult('56', 'test_timeSpan', 'test_sa_period', 'test_sa_damping')
+
+#hcl = HazardCurveList('1', '56')
+
+print hr.id_key    
+print hr.model_id
+print hr.time_span_duration
+print hr.sa_period
+print hr.sa_damping
+
+"""
 # create an Insert object        
-ins = hazard_curve_table.insert()
+ins = hazard_result_table.insert()
 
 # add values to the Insert object
-new_curve = ins.values(model_id=hcg.model_id,
-    end_branch_label=hcg.end_branch_label, _values=hcg.values, imt=hcg.imt,
-    time_span_duration=hcg.time_span_duration, iml_values=hcg.iml_values)
-
+new_curve = ins.values(id_key=hr.id_key, model_id=hr.model_id, 
+    time_span_duration=hr.time_span_duration, sa_period=hr.sa_period, 
+    sa_damping=hr.sa_damping )
+"""
 # create a database connection
 conn = engine.connect()
 # add user to database by executing SQL
--- tests/hazard_curve_to_db_unittest.py
+++ tests/hazard_curve_to_db_unittest.py
@@ -3,19 +3,67 @@
 import os
 import unittest
 
-from openquake.seismicsources import hazard_curve_generator
-from openquake import hazard
+from sqlalchemy import *
+from openquake.seismicsources.hazard_curve_generator import *
 
 
-class HazardCurveTestCase(unittest.TestCase):
+
+class HazardResult(unittest.TestCase):
     
+    def setUp(self):
+        # create the table
+        session = create_session()
+        metadata.drop_all()
+        metadata.create_all()
+    
+    def tearDown(self):
+        # delete the table
+        session.delete(hc)
+        # flush the session
+        session.flush()
+        
+"""   
     def test_simple_hazard_curve_witten_to_db(self):
         
+        def run(stmt):
+            rs = stmt.execute()
+            for row in rs:
+                print row
+        
         #SELECT * FROM hazard_curve_table iml_values  
-        s = hazard_curve_table.select(hazard_curve_table.c.iml_valses)
-        run(s)
+        s = hazard_result_table.select(hazard_result_table.c.model_id == '56')
+        rs = s.execute()
+        results = rs.fetchall()
         
+        self.assertTrue(len(results) > 0)
+        
+
+
         #Return a result set
-        rs = s.execute
         
-        self.assertEqual(s, test_IMLValues)
+        #print rs.rowcount
+        #print s
+        
+        
+        # creating a session
+        session = create_session()
+""" 
+
+    def test_db_accepts_hazard_curve_vauels(self):
+        
+        # creating the hazard curve object
+        hc = HazardResult('1', '56', 'test_timeSpan', 'test_sa_period',
+                    'test_sa_damping')
+            
+                    # end_branch_label == 'TEST'
+        
+        #hc = session.query(HazardResult).selectfirst(hazard_result_table.c.model_id=='56')
+            
+session.save(hc)
+        
+# load the object from database
+hazard_curve_table.select(end_branch_label == 'TEST')
+        # query the database
+        # assert the hazard curve has been stored into the database
+        
+"""     
